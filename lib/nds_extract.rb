c60f317a9278b8d53d2b55ce37f2ee443616e533
John Zhang <jnzhang622@gmail.com>
2020-02-08 04:41:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,8 +7,8 @@ require 'directors_database'
 def gross_for_director(director_data)
   gross_index = 0
   gross = 0
-  while gross_index < director_data[director_index][:movies].size do
-    gross += director_data[director_index][:movies][gross_index][:worldwide_gross]
+  while gross_index < director_data[:movies].size do
+    gross += director_data[:movies][gross_index][:worldwide_gross]
     gross_index += 1
     end
   return gross
