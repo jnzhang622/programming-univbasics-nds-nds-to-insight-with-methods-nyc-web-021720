a566893f1266b03b80c3c7360d4d6c707d343678
John Zhang <jnzhang622@gmail.com>
2020-02-08 04:47:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,13 +19,12 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 
- def directors_totals(nds)
+def directors_totals(nds)
   result = {}
-   director_index = 0
-   while director_index < nds.size do
-     director = nds[director_index]
-     result[director[:name]] = gross_for_director(director)
-     director_index += 1
-   end
-   result
- end
+  director_index = 0
+		while director_index < nds.size do
+      result.store(nds[director_index][:name], gross_for_director)
+			director_index += 1
+		end
+		result
+end
