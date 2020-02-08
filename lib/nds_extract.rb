f281ff5920366f48e5ced1158a96b49c77aff96b
John Zhang <jnzhang622@gmail.com>
2020-02-08 04:49:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,7 @@ def directors_totals(nds)
   result = {}
   director_index = 0
 		while director_index < nds.size do
-      result.store(nds[director_index][:name], gross_for_director)
+      result[nds[director_index][:name]] = gross_for_director(nds[director_index])
 			director_index += 1
 		end
 		result
