3295cd66c38a71b706c48b1096b7adae18e24a17
John Zhang <jnzhang622@gmail.com>
2020-02-08 04:36:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,18 +14,3 @@ def gross_for_director(director_data)
   return gross
 end
 
-# Write a method that, given an NDS creates a new Hash
-# The return value should be like:
-#
-# { directorOne => allTheMoneyTheyMade, ... }
-
-def directors_totals(nds)
-  result = {}
-  director_index = 0
-		while director_index < nds.size do
-      result.store(nds[director_index][:name], gross_for_director)
-			director_index += 1
-		end
-		return result
-  nil
-end
