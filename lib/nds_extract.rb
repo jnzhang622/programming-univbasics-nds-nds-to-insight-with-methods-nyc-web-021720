42743ecca834b7a24d9ea98e39a3af5d7f0b23e8
John Zhang <jnzhang622@gmail.com>
2020-02-08 04:41:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def gross_for_director(director_data)
     gross += director_data[:movies][gross_index][:worldwide_gross]
     gross_index += 1
     end
-  return gross
+  gross
 end
 
 # Write a method that, given an NDS creates a new Hash
